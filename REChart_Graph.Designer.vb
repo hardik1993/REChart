f370c3e607c787ab716957df28117973898c056c
Hardik Patel <hardik.patel@talenenergy.com>
2021-09-27 15:25:10 -0400

    Added some test messages

--- REChart_Graph.Designer.vb
+++ REChart_Graph.Designer.vb
@@ -22,8 +22,8 @@ Partial Class REChart_Graph
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
         Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
@@ -33,10 +33,10 @@ Partial Class REChart_Graph
         '
         'Chart1
         '
-        ChartArea1.Name = "ChartArea1"
-        Me.Chart1.ChartAreas.Add(ChartArea1)
-        Legend1.Name = "Legend1"
-        Me.Chart1.Legends.Add(Legend1)
+        ChartArea2.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend2)
         Me.Chart1.Location = New System.Drawing.Point(54, 28)
         Me.Chart1.Name = "Chart1"
         Me.Chart1.Size = New System.Drawing.Size(960, 570)
@@ -49,7 +49,7 @@ Partial Class REChart_Graph
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 56)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Old Chart Control"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
@@ -58,7 +58,7 @@ Partial Class REChart_Graph
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(101, 56)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "OxyPlot Chart Control"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'PlotView1
